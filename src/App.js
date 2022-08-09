9814f547882c304f76b116c559cddc0e51ca1490
arturogu <arturogu95@gmail.com>
2022-08-09 11:34:29 -0600

    pagination

--- src/App.js
+++ src/App.js
@@ -4,26 +4,41 @@ import './App.css';
 
 function App() {
   const [pokemonList, setPokemonList] = useState([]);
+  const [listOffset, setListOffset] = useState(0);
+  const [searchItem, setSearchItem] = useState('');
   
   useEffect(() => {
     getPokemonList();
-  }, []);
+  }, [listOffset]);
  
   const getPokemonList = async() => {
-    const res = await axios("https://pokeapi.co/api/v2/pokemon?limit=100&offset=0");
+    const res = await axios(`https://pokeapi.co/api/v2/pokemon?limit=100&offset=${listOffset}`);
     setPokemonList(Object.values(res.data.results));
+  }
+
+  const previousPage = () => {
+    setListOffset(listOffset - 100);
+  }
+
+  const nextPage = () => {
+    setListOffset(listOffset + 100);
   }
 
   return (
     <div className="App">
       <h1>Pokedex</h1>
-      <ul>
+      <input onChange={e => setSearchItem(e.target.value)} value={searchItem} type="text"></input>
+      <div>
         { pokemonList.map((pokemon) => {
           return (
-            <li>{pokemon.name}</li>
+            <li key={pokemon.name}>{pokemon.name}</li>
           );
         })}
-      </ul>
+      </div>
+      <div>
+        { listOffset > 0 ? <button onClick={previousPage}>previous</button> : null }
+        { listOffset < 1100 ? <button onClick={nextPage}>next</button> : null }
+      </div>
     </div>
   );
 }
